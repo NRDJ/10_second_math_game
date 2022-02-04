7d9c18be545c9d99c28ce802611a89328e142bad
nrdj <nriedeldj@gmail.com>
2022-02-04 10:37:36 -0300

    input accepts only numbers and start countdown

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    var startCountdown = function () {
+        var timePassed = 0;
+        var timer = null;
+        var startTime;
+        
+        var startTimer = function () {
+          if (!timer) {
+            startTime = 10;
+            secondsPassed = 1;
+            timer = setInterval(function () {
+                timePassed = startTime - secondsPassed;
+                
+                $("#timer").html(timePassed);
+                
+                if (timePassed <= 0) {
+                    stopTimer();
+                }
+
+                secondsPassed += 1;
+            }, 1000); // Executed every 1 second
+          }
+        };
+        
+        var stopTimer = function () {
+          window.clearInterval(timer);
+          timer = null;
+        };
+
+        startTimer();
+        
+    }
+    
+    $('input').keyup(function (event) {
+        if (Number(this.value) || this.value === '0') {
+            startCountdown();
+        };
+    });
+
+});
