ef5cef8af4ea823b433b3790570887aaa2e68df7
nrdj <nriedeldj@gmail.com>
2022-02-05 10:03:57 -0300

    update current score according to the user's input

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
 $(document).ready(function(){
+    var numsAddition;
+    var currentScore = 0;
+
+    var updateCurrentScore = function (action) {
+        var addScoreToDom = function () {
+            $('#current-score').html(`Current Score: ${currentScore}`);
+        }
+
+        // var addOnePoint = function () {
+        //     ++currentScore;
+        // }
+
+        // var removeOnePoint = function () {
+        //     --currentScore;
+        // }
+
+        // var restartPoints = function () {
+        //     currentScore = 0;
+        // }
+
+        switch (action) {
+            case 'add':
+                ++currentScore;
+                break;
+            case 'remove':
+                --currentScore;
+                break;
+            case 'restart':
+                currentScore = 0;
+                break;
+            default:
+                break;
+        }
+
+        addScoreToDom();
+    }
 
     var createNums = function () {
         var addNumToDom = function (numID, num) {
             $(numID).html(num);
+            return num;
         }
         
         var getRandomNum = function() {
             return Math.floor(Math.random() * (9 - 0 + 1) + 0);
         }
 
-        addNumToDom(`#first-num`, getRandomNum());
-        addNumToDom(`#second-num`, getRandomNum());
+        var firstNum = addNumToDom(`#first-num`, getRandomNum());
+        var secondNum = addNumToDom(`#second-num`, getRandomNum());
+        numsAddition =  firstNum + secondNum;
     }
 
-
     var restartGame = function () {
-
+        updateCurrentScore('restart');
     }
 
     var startCountdown = function () {
-        $('input').off();
-        var timePassed = 0;
+        $('input').off('keyup.number');
         var timer = null;
         var startTime;
         
@@ -51,12 +87,30 @@ $(document).ready(function(){
         startTimer();
     }
     
-    $('input').keyup(function (event) {
+    $('input').on('keyup.number', function (e) {
         if (Number(this.value) || this.value === '0') {
             startCountdown();
-            $('input').on();
-        };
+            $('input').on('keyup.number');
+        }
+    });
+
+    $("input").on('keydown', function (e) {
+        var userInput = Number(this.value);        
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            if (userInput === numsAddition) {
+                console.log(`correct!`);
+                createNums();
+                $(this).val('');
+                updateCurrentScore('add');
+
+            } else {
+                console.log(`incorrect!`)
+                $(this).val('');
+                updateCurrentScore('remove');
+            }
+        }
     });
 
     createNums();
 });
+
